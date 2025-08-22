5efbd6def86a1afd92a259768453912ebffa7f30
yakup akyürek <akyurek3466@icloud.com>
2025-08-22 09:02:44 +0300

    Add project files.

---
+++ SensiveProject.EntityLayer/Concrete/NewsLetter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.EntityLayer.Concrete
+{
+    public class NewsLetter
+    {
+        public int NewsletterId { get; set; }
+        public string Mail { get; set; }
+    }
+}
